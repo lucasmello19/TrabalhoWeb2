d6c9e41fa65610fc1d7d69a0a8419993cb859677
Bruno Marques <brunofelipe10@hotmail.com>
2018-11-14 00:04:23 -0200

    Inclusão da Connection Factory
    Inclusão da Connection Factory

---
+++ SAC_WEB2/src/java/Connection/ConnectionFactory.java
@@ -0,0 +1,41 @@
+package Connection;
+
+import java.sql.*;
+
+public class ConnectionFactory {
+    private static final String URL = "jdbc:mysql://localhost:3306/Web2";
+    private static final String DRIVER_CLASS_MYSQL = "com.mysql.jdbc.Driver";
+    private static final String USER = "root";
+    private static final String PASS = "root";
+
+    public static Connection getConnection() {
+        System.out.println("Conectando ao Banco de Dados");
+        try {
+            Class.forName(DRIVER_CLASS_MYSQL);
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    public static void close(Connection conn, Statement stmt, ResultSet rs) {
+        try {
+            if (conn!= null) {
+                conn.close();
+            }
+
+            if (stmt!= null) {
+                stmt.close();
+            }
+
+            if (rs!= null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
